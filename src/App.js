57b0c9d5aef21f2657eafe2cfe945d519c8b3d4c
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-04-18 13:40:59 -0700

    added github footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <br />
+      <footer>
+        This project was coded by Kelsey Murray and is open-sourced on{" "}
+        <a
+          href="https://github.com/artistkelz/week4-react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
